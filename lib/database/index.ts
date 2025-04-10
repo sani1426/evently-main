c2d00118b4b07a887a381d45ab6a1db03dbcc253
saman <sam.karimi727@gmail.com>
2025-04-11 02:18:33 +0330

    update lib/database/index.ts

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -10,7 +10,7 @@ export const connectToDatabase = async () => {
   if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
   cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'train_project',
+    dbName: 'Cluster0',
     bufferCommands: false,
   })
 
