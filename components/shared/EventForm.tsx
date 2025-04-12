d41949185821666195ff16449ccd4f935fa6ae31
saman <sam.karimi727@gmail.com>
2025-04-12 20:17:51 +0330

    update EventForm.tsx

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -79,7 +79,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
 
         if (newEvent) {
           form.reset();
-          router.push(`/events/${newEvent._id}`);
+          router.push(`/`);
         }
       } catch (error) {
         console.log(error);
