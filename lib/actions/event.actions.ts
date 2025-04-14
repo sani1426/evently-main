badf26b062718f684308f7596500bb6577eece5a
saman <sam.karimi727@gmail.com>
2025-04-15 01:42:49 +0330

    update event.actions.ts

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -28,13 +28,14 @@ const populateEvent = (query: any) => {
 }
 
 // CREATE
-export async function createEvent({  event, path }: CreateEventParams) {
+export async function createEvent({ userId, event, path }: CreateEventParams) {
   try {
     await connectToDatabase()
 
+    const organizer = await User.findById(userId)
+    if (!organizer) throw new Error('Organizer not found')
 
-
-    const newEvent = await Event.create({ ...event, category: event.categoryId })
+    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
