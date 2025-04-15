a2c01435bac2ab0fdc86cc9e55c81d7fe8ab69ed
saman <sam.karimi727@gmail.com>
2025-04-15 21:15:49 +0330

    update 2 files and create 1 file

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,21 @@ import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
+import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/config'
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+  weight: ['300','400', '500', '600', '700'],
   variable: '--font-poppins',
 })
 
+
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: `${APP_NAME}. ${APP_SLOGAN}`,
+  },
+  description: APP_DESCRIPTION,
   icons: {
     icon: '/assets/images/logo.svg'
   }
---
+++ lib/config.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Sevently'
+   export const APP_SLOGAN =
+     process.env.NEXT_PUBLIC_APP_SLOGAN || 'Awsome Event Platform.'
+   export const APP_DESCRIPTION =
+     process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+     'Evently is a platform for event management'
