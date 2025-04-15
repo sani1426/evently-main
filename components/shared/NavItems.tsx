0a44bdbc48ec1f58a28e65853df75ea5da3405c0
saman <sam.karimi727@gmail.com>
2025-04-16 00:57:31 +0330

    update NavItems.tsx

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -18,9 +18,9 @@ const NavItems = () => {
             key={link.route}
             className={`text-black ${
               isActive && 'bg-primary-500'
-            } flex-center p-medium-16 whitespace-nowrap rounded-xl px-4 py-2 text-white`}
+            } ${!isActive && 'hover:text-primary-500 transition-all duration-300'} flex-center p-medium-16 whitespace-nowrap rounded-xl px-4 py-2 text-white`}
           >
-            <Link className={`${!isActive && 'hover:text-primary-500 transition-all duration-300'}`} href={link.route}>{link.label}</Link>
+            <Link href={link.route}>{link.label}</Link>
           </li>
         )
       })}
